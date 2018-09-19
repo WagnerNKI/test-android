dbaea1759ee2157e6709e066d574dcbaa6b889b9
Wagner <wagnwnki@gmail.com>
2018-09-18 23:59:53 -0300

    debugging list

--- app/src/main/java/com/example/noely/testeapi/MainActivity.java
+++ app/src/main/java/com/example/noely/testeapi/MainActivity.java
@@ -3,22 +3,18 @@ package com.example.noely.testeapi;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
 public class MainActivity extends AppCompatActivity{
     Button button;
-    TextView Name;
-    String url = "https://api.pokemontcg.io/v1/cards?count=20";
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button)findViewById(R.id.bn);
-        Name = (TextView)findViewById(R.id.name);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
